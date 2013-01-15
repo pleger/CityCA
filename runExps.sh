0d34315ae54fab3bed5b39ff67c0f5d70377bf49
pleger <pleger@gmail.com>
2013-01-15 13:06:29 -0300

    - Adding directories for benchmarks

--- runExps.sh
+++ runExps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup time python Benchmarks.py > log.txt &
+time nohup python Benchmarks.py > log.txt &
 # To work with mail, it is necessary to config sendmail
 echo "Available results :D" | mail -s "ChileCA Available" pleger@gmail.com
